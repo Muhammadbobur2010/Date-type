dbdc96abd7fa293e3e8a46f98977d85b732e0122
Muhammadbobur <sadriddinovmuhammadbobur@gmail.com>
2024-02-23 22:01:00 +0500

    Date tyupe

---
+++ sciript.js
@@ -0,0 +1,10 @@
+let a=prompt('stiring typida malumot kiriting')
+let b=+prompt("number typida malumot kiriting")
+let s=+prompt("boolean tyupida malumot kiriting")
+alert("hamma malumotlarni tog'ri kiritganingizga ishonchingiz komilmi?")
+alert(`
+siz kiritgan ${a} ${typeof a}
+siz kiritgan ${b} ${typeof b}
+siz kiritgan ${s} ${typeof s}
+`
+)
